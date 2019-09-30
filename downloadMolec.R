04271830d73b7cf08b9c4185980718cd19f2c5cc
imanthonny <34114220+imanthonny@users.noreply.github.com>
2019-09-30 18:15:10 -0400

    Update downloadMolec.R

--- downloadMolec.R
+++ downloadMolec.R
@@ -1,4 +1,4 @@
 library(downloader)
 basePath <- "https://ccle.blob.core.windows.net/rnaprocessedzhaleh"
-download(file.path(basePath, "CCLE_Kallisto_0.43.1_processed.zip"), destfile="/pfs/out/CCLE_Kallisto_0.43.1_processed.zip")
-unzip("/pfs/out/CCLE_Kallisto_0.43.1_processed.zip", exdir="/pfs/out/CCLE_Kallisto_0.43.1_processed")
+download(file.path(basePath, "Kallisto_0.43.1_processed.zip"), destfile="/pfs/out/Kallisto_0.43.1_processed.zip")
+unzip("/pfs/out/Kallisto_0.43.1_processed.zip", exdir="/pfs/out/CCLE_Kallisto_0.43.1_processed")
